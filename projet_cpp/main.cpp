f4f8ff65f72c1762d32379d96535a0fbca1bd987
AlexandreN7 <prxo@live.fr>
2014-12-20 23:39:35 +0100

    ajout d'ascii art

--- projet_cpp/main.cpp
+++ projet_cpp/main.cpp
@@ -46,24 +46,49 @@ void menu(void)
 	cin>> choix;
 
 	Equation_diff *simulation;
+
 	switch (choix)
 	{
 		case 1:
 			simulation = new Fonction1();
 			break;
 		case 2:
+			cout<<"----====-------------"<<endl;
+			cout<<"-     R             -"<<endl;
+			cout<<"VE                 ___"<<endl;
+			cout<<"-                C ___"<<endl;
+			cout<<"-                   -"<<endl;
+			cout<<"---------------------"<<endl;
 			simulation = new RC();
 			break;
 		case 3:
+			cout<<"----====-----|>----------====-----"<<endl;
+			cout<<"-     R1            -      R2    -"<<endl;
+			cout<<"VE                 ___           -"<<endl;
+			cout<<"-                C ___           -"<<endl;
+			cout<<"-                   -            -"<<endl;
+			cout<<"----------------------------------"<<endl;
 			simulation = new RC_diode;
 			break;
 		case 4:
 			simulation = new Fonction2();
 			break;
 		case 5:
+			cout<<"----====-----((((((--------"<<endl;
+			cout<<"-     R        L          -"<<endl;
+			cout<<"VE                       ___"<<endl;
+			cout<<"-                      C ___"<<endl;
+			cout<<"-                         -"<<endl;
+			cout<<"---------------------------"<<endl;
 			simulation = new RLC_serie();
 			break;
 		case 6:
+			cout<<"----====---------------(((((((--"<<endl;
+			cout<<"-     R           -      L     - "<<endl;
+			cout<<"VE               ___           - "<<endl;
+			cout<<"-              C ___           - "<<endl;
+			cout<<"-                 -            -"<<endl;
+			cout<<"--------------------------------"<<endl;
 			simulation = new RLC_parallele();
 			break;
 		default :;	
